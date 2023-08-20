a83d422ea0e0e426367f9602aff9e974cc339dbb
karanchhillar <karan.5716403221@ipu.ac.in>
2023-08-20 14:36:29 +0530

    login_singin backend added

--- app/src/main/java/com/example/aritify/login/LoginPage.kt
+++ app/src/main/java/com/example/aritify/login/LoginPage.kt
@@ -1,18 +1,32 @@
 package com.example.aritify.login
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.aritify.MainActivity
 import com.example.aritify.R
 import com.example.aritify.databinding.ActivityLoginPageBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.saadahmedsoft.shortintent.Anim
 import com.saadahmedsoft.shortintent.ShortIntent
 
 class LoginPage : AppCompatActivity() {
 
     private lateinit var binding : ActivityLoginPageBinding
+
+    private lateinit var auth: FirebaseAuth
+    private  lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +44,49 @@ class LoginPage : AppCompatActivity() {
             overridePendingTransition(R.anim.slide_out_left, R.anim.slide_in_right)
         }
 
+        val email = binding.editTextEmail
+        val password = binding.editTextPassword
+
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        binding.signupButton.setOnClickListener {
+
+            val emailInput = email.text.toString()
+            val passwordInput = password.text.toString()
+
+            if (TextUtils.isEmpty(emailInput) || TextUtils.isEmpty(passwordInput) ){
+                Toast.makeText(this, "Cannot be Empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
 
 
+            auth.signInWithEmailAndPassword(emailInput , passwordInput)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful){
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    }
+                    else{
+                        Toast.makeText(this, "${task.exception}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
 
-        binding.loginButton.setOnClickListener {
-            startActivity(Intent(this , MainActivity::class.java))
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+
+        googleSignInClient = GoogleSignIn.getClient(this , gso)
+
+        googleSignInClient.signOut()
+
+
+        binding.imageViewGoogle.setOnClickListener{
+            signInGoogle()
         }
 
 
@@ -62,5 +114,41 @@ class LoginPage : AppCompatActivity() {
             // Start fade-out animation
 //            binding.newHereRegisterText.startAnimation(fadeOutAnimation)
 //        }
+    }
+
+    private fun signInGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+        if (result.resultCode == Activity.RESULT_OK){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+    }
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUI(account)
+            }else{
+                Toast.makeText(this , task.exception.toString() , Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken , null)
+
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener{
+                if (it.isSuccessful){
+                    val intent : Intent = Intent(this , MainActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(this , it.exception.toString() , Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/example/aritify/login/RegisterPage.kt
+++ app/src/main/java/com/example/aritify/login/RegisterPage.kt
@@ -1,15 +1,31 @@
 package com.example.aritify.login
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.aritify.MainActivity
 import com.example.aritify.R
 import com.example.aritify.databinding.ActivityRegisterPageBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class RegisterPage : AppCompatActivity() {
 
     private lateinit var binding : ActivityRegisterPageBinding
+
+    private lateinit var auth: FirebaseAuth
+    private  lateinit var googleSignInClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterPageBinding.inflate(layoutInflater)
@@ -25,6 +41,90 @@ class RegisterPage : AppCompatActivity() {
             startActivity(intent)
             overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right)
         }
+
+        val email : EditText = binding.editTextEmail1
+        val password : EditText = binding.editTextPassword1
+        val confirmPassword : EditText = binding.editTextConfirmPassword
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.signupButton.setOnClickListener {
+            val emailInput = email.text.toString()
+            val passwordInput = password.text.toString()
+            val confirmPasswordInput = confirmPassword.text.toString()
+
+            if (TextUtils.isEmpty(emailInput) || TextUtils.isEmpty(passwordInput) || TextUtils.isEmpty(confirmPasswordInput)) {
+                Toast.makeText(this, "Cannot be Empty", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (passwordInput.length < 6) {
+                Toast.makeText(this, "Password should be more than 6 letters", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (passwordInput != confirmPasswordInput) {
+                Toast.makeText(this, "Both are not same", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(emailInput , passwordInput)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful){
+                        Toast.makeText(this, "New Id created", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                    }
+                    else{
+                        Toast.makeText(this, "${task.exception}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+
+        googleSignInClient = GoogleSignIn.getClient(this , gso)
+
+        googleSignInClient.signOut()
+
+
+        binding.imageViewGoogle1.setOnClickListener{
+            signInGoogle()
+        }
+    }
+
+    private fun signInGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        launcher.launch(signInIntent)
+    }
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+        if (result.resultCode == Activity.RESULT_OK){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleResults(task)
+        }
+    }
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        if (task.isSuccessful){
+            val account : GoogleSignInAccount? = task.result
+            if (account != null){
+                updateUI(account)
+            }else{
+                Toast.makeText(this , task.exception.toString() , Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken , null)
+
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener{
+                if (it.isSuccessful){
+                    val intent : Intent = Intent(this , MainActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(this , it.exception.toString() , Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
 //    override fun onBackPressed() {
